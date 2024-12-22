7075913a1a539a77fbe224b4f4ddf06a4492c369
dilmohan <macbookpro@MacBooks-MacBook-Pro.local>
2024-12-22 11:16:19 +0530

    test deploy

--- backend/server.js
+++ backend/server.js
@@ -46,8 +46,7 @@ app.use(cors({
 
 mongoose
   .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
